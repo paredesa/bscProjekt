5a3486c0a9a3d17d7bc15d90d54fbfc485517c5b
Andrés Paredes <paradesadnres@gmail.com>
2019-05-26 19:52:09 +0200

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+// eslint-disable-next-line no-undef
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+// eslint-disable-next-line no-undef
+module.exports = {
+  output: {
+    publicPath: "/"
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1
+            }
+          }
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1
+            }
+          },
+          {
+            loader: "less-loader"
+          }
+        ]
+      },
+      /* {
+        test: /\.svg$/,
+        loader: "svg-inline-loader"
+      }, */
+      {
+        test: /\.(png|jpg|gif|ico)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name].[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      favicon: "./public/favicon.ico",
+      filename: "./index.html"
+    })
+  ]
+};
